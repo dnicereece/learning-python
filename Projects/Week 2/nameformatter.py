b64bc5ab60c68911cd4ea189c1809a72f98e07f5
dnicereece <dnice.reece@gmail.com>
2025-08-27 12:11:52 -0400

    feat: create program for formatting user name

---
+++ Projects/Week 2/nameformatter.py
@@ -0,0 +1,11 @@
+# Greet user and ask for their name
+print("Hello! What is your name?")
+
+# Get user input
+name = input()
+
+# Convert string to title case (all words capitalized)
+name.title()
+
+# Print greeting with the user's name in title case
+print("Hello, " + name.title() + "!")
