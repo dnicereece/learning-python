460a03cdfcff0529954348a7db438f7ac9228470
dnicereece <dnice.reece@gmail.com>
2025-08-29 12:37:29 -0400

    feat: add hello it's me pset

---
+++ Projects/Week 2/CS50 PSet1/hello.py
@@ -0,0 +1,3 @@
+# Get user name and format it
+name = input("What is your name? ").strip().title() # Strip whitespace and format as title case
+print(f"Hello, {name}") # Greet the user with their formatted name
